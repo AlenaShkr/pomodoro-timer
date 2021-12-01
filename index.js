a0d9db90beafa1fe2136b9ce95b89062b5afdb35
”AlenaShkr” <elalshkr@gmail.com>
2021-12-02 01:21:44 +0300

    pomodoro: add basic for creation class in js file

--- index.js
+++ index.js
@@ -1,3 +1,17 @@
+class Timer {
+    constructor() {
+        this.duration = 15*60*60;
+    }
+star() {
+
+}
+stop() {
+
+}
+edit() {
+
+}
+}
 window.onload = function load() {
-  
+  const pomodoroTimer = new Timer;
 };
